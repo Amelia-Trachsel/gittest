41b61a28188cc9cfaac76d55f7e27cfe33a4eada
Amelia-Trachsel <amelia.trachsel@gmail.com>
2018-01-24 12:26:03 -0800

    JavaScript Declare
    Adding Readmes

---
+++ JavaScript Declare/Javascript Declare.js
@@ -0,0 +1,17 @@
+var Number1 = 3.14;
+var Number2 = 3.75;
+var Number3 = 4.22;
+var Number4 = 7.33;
+
+var string1 = "sandwich";
+var string2 = "basketball";
+var string3 = "house";
+
+var boolean1 = true;
+var boolean2 = false;
+
+var undefined1 
+
+console.log (Number1, Number2, Number3, Number4, string1, string2, string3, boolean1, boolean2, undefined1);
+
+
